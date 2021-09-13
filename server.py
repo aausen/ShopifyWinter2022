3c7a24aad980c3fbc4262496f90f7a63806b9285
Annie Ausen <ausen.annie@gmail.com>
2021-09-13 15:15:03 -0500

    initial set-up

--- server.py
+++ server.py
@@ -1,1 +1,18 @@
-adding for a test
+"""Server for Shopify Image Repository"""
+
+from flask import Flask, render_template, request
+from model import connect_to_db, db
+import os
+
+app = Flask(__name__)
+app.secret_key = os.environ['secret_key']
+
+
+
+
+
+
+if __name__ == '__main__':
+    connect_to_db(app)
+
+    app.run(host='0.0.0.0', debug = True)
