5b2115a29110e1123fcb35ee7aecd84c69ec5311
Annie Ausen <ausen.annie@gmail.com>
2021-09-16 15:10:48 -0500

    work on delete and showing img

--- crud.py
+++ crud.py
@@ -12,7 +12,14 @@ def create_image(image_name):
 
     return image
 
+def get_all_images():
+    """Get all the images from the database."""
+
+    return Image.query.all()
+
 def delete_image_from_db(image_id):
+    """Delete an image."""
+
     del_image = Image.query.filter(Image.image_id == image_id).first()
     db.session.delete(del_image)
     db.session.commit()
--- server.py
+++ server.py
@@ -5,8 +5,10 @@ from werkzeug.utils import secure_filename
 from model import connect_to_db, db
 import crud
 import os
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.jinja_env.undefined = StrictUndefined
 app.secret_key = os.environ['secret_key']
 ALLOWED_EXTENTIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
@@ -16,32 +18,49 @@ def allowed_filename(filename):
 
 @app.route("/", methods=['GET', 'POST'])
 def upload_file():
-    print('getting stuck')
+
+    all_img = crud.get_all_images()
+
+    single_image = []
+    for item in all_img:
+        img_id = item.image_id
+        img_name = item.image_name
+
+        single_image.append((img_id, img_name))
+
     if request.method == 'POST':
-        print('getting stuck 1')
         if 'file' not in request.files:
-            print('getting stuck 2')
             flash("No file part.")
             return redirect(request.url)
         file = request.files['file']
 
         if file.filename == '':
-            print('getting stuck 3')
             flash('No selected file.')
             return redirect(request.url)
         if file and allowed_filename(file.filename):
-            print('getting stuck 4')
             filename = secure_filename(file.filename)
             crud.create_image(filename)
             flash('Image added')
-            return render_template('index.html')
-    return render_template("index.html")
+            return render_template('index.html', 
+                                    single_image = single_image)
+    return render_template("index.html", 
+                            single_image = single_image)
 
-@app.route("/")
-def show_index():
-    """View index."""
+# @app.route("/")
+# def show_index():
+#     """View index."""
 
-    return render_template("index.html")
+#     all_img = crud.get_all_images()
+
+#     single_image = []
+#     for item in all_img:
+#         img_id = item.image_id
+#         img_name = item.image_name
+
+#         single_image.append((img_id, img_name))
+
+#     return render_template("index.html", 
+#                             single_image = single_image)
 
 
 
